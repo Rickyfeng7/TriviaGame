57befb7ffe4439667534ef3b4e5256641672455d
Ricky Feng <34487908+Rickyfeng7@users.noreply.github.com>
2018-01-27 15:22:47 -0800

    add updating javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,47 @@
+		var timeLeft
+
+		function timer() {
+			alert("BOOM");
+		}
+		setTimeout(timer, 10000);
+		console.log(timer);
+
+		var quiz = [
+			{	question:("what NFL franchise has the most superbowl wins?"),
+				option:["New England Patriots", "Dallas CowBoys", "Denver Broncos", "Pittsburgh Steelers" ],
+				answer:[3],
+
+			},
+			{	question:("what NFL franchise has the most loss in the superbowls?"),
+				option:["New England Patriots", "Dallas CowBoys", "Denver Broncos", "Pittsburgh Steelers" ],
+				answer:[2],
+			},
+			{	question:("what NFL franchise has the most superbowl appearences?"),
+				option:["New England Patriots", "Dallas CowBoys", "Denver Broncos", "Pittsburgh Steelers" ],
+				answer:[0],
+			},
+
+
+		]
+				console.log(quiz[0].question);
+				console.log(quiz[0].option);
+				console.log(quiz[0].answer);
+
+				console.log(quiz[1].question);
+				console.log(quiz[1].option);
+				console.log(quiz[1].answer);
+
+				console.log(quiz[2].question);
+				console.log(quiz[2].option);
+				console.log(quiz[2].answer);
+
+				console.log(quiz[3].question);
+				console.log(quiz[3].option);
+				console.log(quiz[3].answer);
+
+
+
+
+
+
+
